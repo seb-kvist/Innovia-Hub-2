e2e93d5be2541f8151d6145e5a817b4c41701340
Edwin Cassman <edwin@cassman.se>
2025-09-02 14:00:57 +0200

    database test

--- Backend/DbContext/AppDbContext.cs
+++ Backend/DbContext/AppDbContext.cs
@@ -2,7 +2,15 @@ using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>().HasData(
+            new User { Id = 1, Name = "test" }
+        );
+
+    }
 }
---
+++ Backend/Migrations/20250902115541_User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "test" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
