b75929e15e7a0dcc027361a4984ae290c4ba2f5a
JehadAbdou <jood111ao@hotmail.com>
2025-09-03 12:21:26 +0200

    Register

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -1,11 +1,38 @@
 using System;
 using Backend.DTOs.Auth;
+using Backend.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Backend.DTOs.Auth;
 
 namespace Backend.Controllers;
 
-public class AuthController
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
 {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+
+    public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+
+    }
+    
+      [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] DTORegisterRequest model)
+    {
+        var user = new User { UserName = model.Name, Email = model.Email };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+        if (!result.Succeeded)
+            return BadRequest(result.Errors);
+
+        return Ok(new { Message = "User registered successfully" });
+    }
+
 
 }
