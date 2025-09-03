b5f6ec4180f87e12c4fc77ae7f7efcd0b05c07ea
MilicaBl <milab.code@gmail.com>
2025-09-03 08:53:38 -0400

    jwt token setup

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Backend.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Backend.DTOs.Auth;
+using API;
 
 namespace Backend.Controllers;
 
@@ -13,18 +14,29 @@ public class AuthController : ControllerBase
 {
     private readonly UserManager<IdentityUser> _userManager;
     private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly JwtToken _jwtToken;
 
-
-    public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, JwtToken jwtToken)
     {
         _userManager = userManager;
         _signInManager = signInManager;
+        _jwtToken = jwtToken;
 
     }
-    
-      [HttpPost("register")]
+
+    [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] DTORegisterRequest model)
     {
+        if (model == null)
+        {
+            return BadRequest(new { message = "Invalid request" });
+        }
+        var existingUser = await _userManager.FindByEmailAsync(model.Email);
+        if (existingUser != null)
+        {
+            return Conflict(new { message = "Username is already taken" });
+        }
+
         var user = new User { UserName = model.Name, Email = model.Email };
 
         var result = await _userManager.CreateAsync(user, model.Password);
@@ -33,6 +45,5 @@ public class AuthController : ControllerBase
 
         return Ok(new { Message = "User registered successfully" });
     }
-
-
+    
 }
