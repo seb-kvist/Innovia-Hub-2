5d2e27c7e1987e8200004fb90d7f8840eb8d9f1e
Sebastian Kvist <sebastian_kvist@hotmail.com>
2025-09-03 11:32:36 +0200

    adjusted the identity import away from <int>

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 namespace Backend.Models;
 
 /// Identity-användare. Ärver från IdentityUser<int> vilket ger Id, Email, PasswordHash m.m.
-public class User : IdentityUser<int>
+public class User : IdentityUser
 {
     public string Name { get; set; }
 }
