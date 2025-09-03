7ca949dcf0065638fdfaab04a6a38f15e57181fc
MilicaBl <milab.code@gmail.com>
2025-09-03 11:47:18 -0400

    booking classes

---
+++ Backend/Models/Booking.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Backend.Models;
+
+public class Booking
+{
+     public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public required string TimeSlot { get; set; } // (08–10)
+
+    // Relations
+    public required string UserId { get; set; }
+    public User User { get; set; } = null!;
+
+    public int ResourceId { get; set; }
+    public Resource Resource { get; set; } = null!;
+}
