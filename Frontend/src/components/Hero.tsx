9b1700a50bca63137f5d377ad44524ca8f691764
Sebastian Kvist <sebastian_kvist@hotmail.com>
2025-09-10 10:49:51 +0200

    changes to css

--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Hero.css";
+import { Link } from "react-router-dom";
 
 interface HeroProps {
     isLoggedIn: boolean;
@@ -8,7 +9,6 @@ interface HeroProps {
 const Hero: React.FC<HeroProps> = ({ isLoggedIn }) => {
     return (
         <section className="hero">
-            <img src="/img/innovialogo.png" alt="Innovia logo" className="hero-logo" style={{width: 106, height: 106, marginBottom: 12}} />
             <h2>Välkommen till</h2>
             <h1>Innovia Hub</h1>
             <p>
@@ -18,7 +18,7 @@ const Hero: React.FC<HeroProps> = ({ isLoggedIn }) => {
 
             {!isLoggedIn && (
                 // Om användaren inte är inloggad → visa call to action knapp
-                <button className="btn-cta">BLI MEDLEM IDAG!</button>
+                <Link to="/login" className="btn-cta">BLI MEDLEM IDAG!</Link>
             )}
         </section>
     );
