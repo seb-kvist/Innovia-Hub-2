d97d5f5477f37a825616566df683f54c7e89fbb0
JehadAbdou <jood111ao@hotmail.com>
2025-09-11 21:53:03 +0200

    signalR api changes

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5022/api";
 const api = axios.create({
-  baseURL: "http://localhost:5022/api",
+  baseURL: API_BASE_URL,
 });
 
 //AUTH - Register
